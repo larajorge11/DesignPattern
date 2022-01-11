a7b2c3f1f9b25442438aafd70785aec4a38b3403
Jorge Lara <jorge.lara@morningstar.com>
2022-01-10 19:41:36 -0500

    feat(Replacing adapter class to interface)

--- src/main/java/structural/adapter/example02/Client.java
+++ src/main/java/structural/adapter/example02/Client.java
@@ -3,11 +3,11 @@ package structural.adapter.example02;
 public class Client {
 
   public static void main(String[] args) {
-    BaliHotelAdapter baliHotel = new BaliHotelAdapter(new BaliHotel());
+    HotelTarget baliHotel = new BaliHotelAdapter(new BaliHotel());
     baliHotel.printAvailableRooms();
     baliHotel.bookByRoomNumber(203);
 
-    CancunHotelAdapter cancunHotel = new CancunHotelAdapter(new CancunHotel());
+    HotelTarget cancunHotel = new CancunHotelAdapter(new CancunHotel());
     cancunHotel.printAvailableRooms();
     cancunHotel.bookByRoomNumber(763);
   }
